915aa27be48a888d858f7b30788d7a019cd00704
Elvin Chong <elvinchong@seekasia.com>
2018-10-17 10:28:15 +0800

    some edits

---
+++ src/Welcome/welcome.js
@@ -0,0 +1,21 @@
+import React, { Component, Fragment } from 'react';
+
+export default class Welcome extends Component {
+    render() {
+        return (
+            <Fragment>
+                <p>
+                Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Learn React
+                </a>
+            </Fragment>
+        )
+    }
+};
