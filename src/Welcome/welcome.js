5936e59a233659ad987c19a18e37246f28d16d77
Elvin Chong <elvinchong@seekasia.com>
2018-10-17 11:05:19 +0800

    added eslint and rules

--- src/Welcome/welcome.js
+++ src/Welcome/welcome.js
@@ -1,21 +1,21 @@
 import React, { Component, Fragment } from 'react';
 
 export default class Welcome extends Component {
-    render() {
+    render () {
         return (
             <Fragment>
                 <p>
-                Edit <code>src/App.js</code> and save to reload.
+                    Edit <code>src/App.js</code> and save to reload.
                 </p>
                 <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
+                    className='App-link'
+                    href='https://reactjs.org'
+                    target='_blank'
+                    rel='noopener noreferrer'
                 >
-                Learn React
+                    Learn React
                 </a>
             </Fragment>
-        )
+        );
     }
-};
+}
