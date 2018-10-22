8722151dd05671ad60933ccd9938f81d6faf8003
ElvinChong <cjchong12345@gmail.com>
2018-10-23 00:21:59 +0800

    Updates

---
+++ precache-manifest.e2e152bac4dd28e8d8767ba9e9c4ed92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-project/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "0a54ebb76cbb06400fd1",
+    "url": "/react-project/static/js/runtime~main.0a54ebb7.js"
+  },
+  {
+    "revision": "5d933563ad389589cafd",
+    "url": "/react-project/static/js/main.5d933563.chunk.js"
+  },
+  {
+    "revision": "513ecec4e5996697e997",
+    "url": "/react-project/static/js/1.513ecec4.chunk.js"
+  },
+  {
+    "revision": "5d933563ad389589cafd",
+    "url": "/react-project/static/css/main.c152c134.chunk.css"
+  },
+  {
+    "revision": "cca860a18d3170f42a7f1542fde1ae62",
+    "url": "/react-project/index.html"
+  }
+];
