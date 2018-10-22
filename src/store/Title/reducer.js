9bc0e286f850eb11af4db79fda0bf8f9aa8bbbb3
ElvinChong <cjchong12345@gmail.com>
2018-10-22 22:43:35 +0800

    added change title for pokemon route

--- src/store/Title/reducer.js
+++ src/store/Title/reducer.js
@@ -1,5 +1,5 @@
-import { WELCOME, TODOLIST, USER, USERDETAIL} from '../Route/routeNames';
-import { DEFAULTTITLE, WELCOMETITLE, TODOLISTTITLE, USERTITLE, USERDETAILTITLE } from './titleNames';
+import { WELCOME, TODOLIST, USER, USERDETAIL, POKEMON} from '../Route/routeNames';
+import { DEFAULTTITLE, WELCOMETITLE, TODOLISTTITLE, USERTITLE, USERDETAILTITLE, POKEMONTITLE } from './titleNames';
 
 const titleReducer = (state = DEFAULTTITLE, action = {}) => {
     switch (action.type) {
@@ -11,6 +11,9 @@ const titleReducer = (state = DEFAULTTITLE, action = {}) => {
         return USERTITLE;
     case USERDETAIL:
         return `${USERDETAILTITLE} - ${action.payload.userName}`;
+    case POKEMON: {
+        return POKEMONTITLE;
+    }
     default:
         return state;
     }
